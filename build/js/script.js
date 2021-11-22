34e60c9f92cebbcdfa1be5b3a40ab1ab4611f31c
Маша <mbanakova@yandex.ru>
2021-11-23 00:44:45 +0300

    git init

---
+++ build/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let toggle = document.querySelector(".toggle");
+let menu = document.querySelector(".menu");
+
+toggle.onclick = function () {
+  menu.classList.toggle("active");
+};
+//# sourceMappingURL=script.js.map
